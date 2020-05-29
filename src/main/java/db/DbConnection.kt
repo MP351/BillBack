b1a6dd971e4aad154926beba3ae6b509a2270b39
MP351 <maxpayne351@gmail.com>
2020-05-29 14:26:39 +0300

    DB changes

--- src/main/java/db/DbConnection.kt
+++ src/main/java/db/DbConnection.kt
@@ -4,7 +4,6 @@ import org.jetbrains.exposed.sql.Database
 
 object DbSettings {
     val db by lazy {
-        Database.connect("jdbc:postgresql://10.111.0.201:5432/viptec_billing",
-                "org.postgresql.Driver", "sber_backend", "backendPassword")
+
     }
 }
